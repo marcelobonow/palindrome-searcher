079131ae6d200e5f6e1a82273810e0fefb6abf1e
Marcelo Ott <marcelobonowott@hotmail.com>
2022-08-25 13:53:36 -0300

    Primeira versão multiplo buffer

--- src/main.cpp
+++ src/main.cpp
@@ -1,28 +1,30 @@
 ﻿#include <iostream>
 #include <fstream>
 
+void Input();
 int FindPalindrome(char*, unsigned int, unsigned int);
 void PrintPalindrome(char*, unsigned int, unsigned int);
 
+unsigned int palindromeSize = 0;
+unsigned int bufferMultiplier = 1;
+
 int main() {
 	setlocale(LC_ALL, "");
-	std::ifstream file("data/test.txt");
+	std::ifstream file("data/test4.txt");
 
-	const auto bufferSize = 2000000;
-	std::cout << "Digite o tamanho do palindromo: ";
-	std::string palindromeSizeInput;
-	std::cin >> palindromeSizeInput;
-	unsigned int palindromeSize = std::atoi(palindromeSizeInput.c_str());
-	while (palindromeSize == 0 && strcmp(palindromeSizeInput.c_str(), "0") != 0) {
-		std::cout << "Valor digitado inválido, tente novamente: ";
-		std::cin >> palindromeSizeInput;
-		palindromeSize = std::atoi(palindromeSizeInput.c_str());
-	}
-	char* buffer = (char*)malloc(sizeof(char) * bufferSize);
-	while (file.get(buffer, bufferSize)) {
+	///Tratamento de input
+	Input();
+
+
+	/// O palindromo pode ser cortado pela divisória do buffer, então precisamos alocar um offset
+	///No pior caso, o palindromo cortado precisa de 2*palindromeSize - 1
+	auto bufferSize = 2 * palindromeSize * bufferMultiplier;
+	auto bufferPadding = (2 * palindromeSize - 1);
+	char* buffer = (char*)malloc(sizeof(char) * (bufferSize + bufferPadding));
+	while (file.get(buffer, bufferMultiplier)) {
 		std::cout << "Buffer" << "\n";
 		///TODO: Append com os ultimos dígitos do buffer anterior + os primeiros dígitos do próximo buffer e envia para buscar o palindromo
-		auto palindromeIndex = FindPalindrome(buffer, bufferSize, palindromeSize);
+		auto palindromeIndex = FindPalindrome(buffer, bufferMultiplier, palindromeSize);
 		if (palindromeIndex >= 0)
 		{
 			std::cout << "É palindromo no index " << palindromeIndex << "\n";
@@ -30,6 +32,28 @@ int main() {
 		}
 		else
 			std::cout << "Não é palindromo\n";
+	}
+}
+
+void Input() {
+	std::cout << "Digite o tamanho do palindromo: ";
+	std::string palindromeSizeInput;
+	std::cin >> palindromeSizeInput;
+	palindromeSize = std::atoi(palindromeSizeInput.c_str());
+	while (palindromeSize <= 0 && strcmp(palindromeSizeInput.c_str(), "0") != 0) {
+		std::cout << "Valor digitado inválido, tente novamente: ";
+		std::cin >> palindromeSizeInput;
+		palindromeSize = std::atoi(palindromeSizeInput.c_str());
+	}
+
+	std::cout << "Digite o multiplicador do buffer: ";
+	std::string bufferMultiplierInput;
+	std::cin >> bufferMultiplierInput;
+	bufferMultiplier = std::atoi(bufferMultiplierInput.c_str());
+	while (bufferMultiplier <= 0 && strcmp(bufferMultiplierInput.c_str(), "0") != 0) {
+		std::cout << "Valor digitado inválido, tente novamente: ";
+		std::cin >> bufferMultiplierInput;
+		bufferMultiplier = std::atoi(bufferMultiplierInput.c_str());
 	}
 }
 
@@ -57,7 +81,7 @@ int FindPalindrome(char* buffer, unsigned int bufferSize, unsigned int palindrom
 		///0 compara com 5, 1 compara com 4, 2 compara com 3
 
 		for (int radarIndex = 0; radarIndex < palindromeSize; radarIndex++) {
-			debugFile << "Verificando se " << radar[radarIndex] << " = " << radar[radarLength - 1 - radarIndex] << ". Buffer Index " << bufferIndex << "\n";
+			/*debugFile << "Verificando se " << radar[radarIndex] << " = " << radar[radarLength - 1 - radarIndex] << ". Buffer Index " << bufferIndex << "\n";*/
 			auto leftDigit = radar[radarIndex];
 			auto rightDigit = radar[radarLength - 1 - radarIndex];
 
