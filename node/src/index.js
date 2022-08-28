dc56ca1b02fba2fad8add7f7df56bd5fd9083c9e
Marcelo Ott <marcelobonowott@hotmail.com>
2022-08-28 17:45:48 -0300

    Melhoria no output

--- node/src/index.js
+++ node/src/index.js
@@ -2,6 +2,7 @@ const StartSearching = require("./piDigits.js");
 const { FindPrimePalindrome } = require("./piDigits");
 
 let primePalindromes = [];
+let smallerDigitFound = -1;
 
 async function GetMultipleDigits() {
   ///TODO: Fazer em multithread
@@ -13,22 +14,36 @@ async function GetMultipleDigits() {
   const promises = batchesIndex.map(batchIndex => GetDigits(batchIndex, batchSize, apiDigitSize, palindromeSize, offset));
   await Promise.all(promises);
   console.log("Finalizou");
+  for (const prime of primePalindromes) {
+    console.log('----------------------------');
+    console.log("Digito: " + prime.digit);
+    console.log("Numero: " + prime.number);
+  }
+  console.log('----------------------------');
 }
 
 async function GetDigits(batchIndex, batchSize, apiDigitSize, palindromeSize, offset) {
-  for (let i = 0; i < 1000; i++) {
+  let startDigit = 0;
+  for (let i = 0; i < 1000 && (smallerDigitFound < 0 || smallerDigitFound > startDigit); i++) {
     const startBatchDigit = batchSize * apiDigitSize * i;
     const overlap = (batchSize * batchIndex + i) * (palindromeSize - 1);
-    const startDigit = Math.max(0, (startBatchDigit + (apiDigitSize * batchIndex) - overlap + offset));
+    startDigit = Math.max(0, (startBatchDigit + (apiDigitSize * batchIndex) - overlap + offset));
     console.log(`Busca iniciando em ${startDigit} e terminando em ${startDigit + apiDigitSize}`);
     const result = await FindPrimePalindrome(startDigit, apiDigitSize, palindromeSize);
     console.log(result);
     if (result != "Not found") {
       console.log("RESULTADO! " + result);
 
-      console.log("Digito com palíndromo primo: " + result.digit);
+      const absoluteDigit = (result.digit + startDigit);
+      console.log("Digito com palíndromo primo: " + absoluteDigit);
       console.log("palíndromo primo: " + result.number);
-      await sleep(30000);
+      if (smallerDigitFound > absoluteDigit || smallerDigitFound < 0)
+        smallerDigitFound = absoluteDigit;
+
+      primePalindromes.push({
+        digit: absoluteDigit,
+        number: result.number,
+      });
       break;
     }
   }
